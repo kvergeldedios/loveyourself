1fa17d76ed301086f2e926dd700760944fbdfeb3
kvergeldedios <58003111+kvergeldedios@users.noreply.github.com>
2020-05-30 10:48:45 +0100

    Add files via upload
    Final web layout

---
+++ formscript.js
@@ -0,0 +1,16 @@
+
+
+
+	// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+
+var home = document.getElementById("homepage");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
